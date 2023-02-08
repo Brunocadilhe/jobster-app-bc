6185a89862e281c38a4fa2faec93d432ec35a955
Brunocadilhe <cadilheb@gmail.com>
2023-02-08 14:58:31 +0200

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import BigSideBar from './BigSideBar'
+import FormRow from './FormRow'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import SmallSideBar from './SmallSideBar'
+import FormRowSelect from './FormRowSelect'
+import JobsContainer from './JobsContainer'
+import SearchContainer from './SearchContainer'
+import StatsContainer from './StatsContainer'
+import ChartsContainer from './ChartsContainer'
+
+export {
+  Logo,
+  FormRow,
+  Navbar,
+  SmallSideBar,
+  BigSideBar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+  StatsContainer,
+  ChartsContainer,
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Error from './Error'
+import Landing from './Landing'
+import Register from './Register'
+import ProtectedRoute from './ProtectedRoute'
+import FormRowSelect from '../components/FormRowSelect'
+
+export { Landing, Error, Register, ProtectedRoute, FormRowSelect }
